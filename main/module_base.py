c5de502dfec91f18ee71449fdced3a51b119b120
Giorgio Mariani <giorgiomariani94@gmail.com>
2023-03-28 13:59:29 +0000

    Fix error while storing jamendo checkpoint

--- main/module_base.py
+++ main/module_base.py
@@ -98,8 +98,8 @@ class WebDatasetDatamodule(pl.LightningDataModule):
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.shuffle_size = shuffle_size
-        self.save_hyperparameters()
-        
+        #self.save_hyperparameters()
+
         self.train_dataset = train_dataset
         self.val_dataset = val_dataset
 
@@ -219,73 +219,73 @@ def get_wandb_logger(trainer: Trainer) -> Optional[WandbLogger]:
     return None
 
 
-class SampleLogger(Callback):    
-    def __init__(    
-        self,    
-        num_items: int,    
-        channels: int,    
-        sampling_rate: int,    
-        length: int,    
-        sampling_steps: List[int],    
-        diffusion_schedule: Schedule,    
-        diffusion_sampler: Sampler,    
-    ) -> None:    
-        self.num_items = num_items    
-        self.channels = channels    
-        self.sampling_rate = sampling_rate    
-        self.length = length    
-        self.sampling_steps = sampling_steps    
-        self.diffusion_schedule = diffusion_schedule    
-        self.diffusion_sampler = diffusion_sampler    
-    
-        self.log_next = False    
-    
-    def on_validation_epoch_start(self, trainer, pl_module):    
-        self.log_next = True    
-    
-    def on_validation_batch_start(    
-        self, trainer, pl_module, batch, batch_idx, dataloader_idx    
-    ):    
-        if self.log_next:    
-            self.log_sample(trainer, pl_module, batch)    
-            self.log_next = False    
-    
-    @torch.no_grad()    
-    def log_sample(self, trainer, pl_module, batch):    
-        is_train = pl_module.training    
-        if is_train:    
-            pl_module.eval()    
-    
-        wandb_logger = get_wandb_logger(trainer).experiment    
-        model = pl_module.model    
-    
-        # Get start diffusion noise    
-        noise = torch.randn(    
-            (self.num_items, self.channels, self.length), device=pl_module.device    
-        )    
-    
-        for steps in self.sampling_steps:    
-    
-            samples = model.sample(    
-                noise=noise,    
-                sampler=self.diffusion_sampler,    
-                sigma_schedule=self.diffusion_schedule,    
-                num_steps=steps,    
-            )    
-            samples = rearrange(samples, "b c t -> b t c").detach().cpu().numpy()    
-    
-            wandb_logger.log(    
-                {    
-                    f"sample_{idx}_{steps}": wandb.Audio(    
-                        samples[idx],    
-                        caption=f"Sampled in {steps} steps",    
-                        sample_rate=self.sampling_rate,    
-                    )    
-                    for idx in range(self.num_items)    
-                }     
-            )         
-                      
-        if is_train:    
+class SampleLogger(Callback):
+    def __init__(
+        self,
+        num_items: int,
+        channels: int,
+        sampling_rate: int,
+        length: int,
+        sampling_steps: List[int],
+        diffusion_schedule: Schedule,
+        diffusion_sampler: Sampler,
+    ) -> None:
+        self.num_items = num_items
+        self.channels = channels
+        self.sampling_rate = sampling_rate
+        self.length = length
+        self.sampling_steps = sampling_steps
+        self.diffusion_schedule = diffusion_schedule
+        self.diffusion_sampler = diffusion_sampler
+
+        self.log_next = False
+
+    def on_validation_epoch_start(self, trainer, pl_module):
+        self.log_next = True
+
+    def on_validation_batch_start(
+        self, trainer, pl_module, batch, batch_idx, dataloader_idx
+    ):
+        if self.log_next:
+            self.log_sample(trainer, pl_module, batch)
+            self.log_next = False
+
+    @torch.no_grad()
+    def log_sample(self, trainer, pl_module, batch):
+        is_train = pl_module.training
+        if is_train:
+            pl_module.eval()
+
+        wandb_logger = get_wandb_logger(trainer).experiment
+        model = pl_module.model
+
+        # Get start diffusion noise
+        noise = torch.randn(
+            (self.num_items, self.channels, self.length), device=pl_module.device
+        )
+
+        for steps in self.sampling_steps:
+
+            samples = model.sample(
+                noise=noise,
+                sampler=self.diffusion_sampler,
+                sigma_schedule=self.diffusion_schedule,
+                num_steps=steps,
+            )
+            samples = rearrange(samples, "b c t -> b t c").detach().cpu().numpy()
+
+            wandb_logger.log(
+                {
+                    f"sample_{idx}_{steps}": wandb.Audio(
+                        samples[idx],
+                        caption=f"Sampled in {steps} steps",
+                        sample_rate=self.sampling_rate,
+                    )
+                    for idx in range(self.num_items)
+                }
+            )
+
+        if is_train:
             pl_module.train()
 
 
